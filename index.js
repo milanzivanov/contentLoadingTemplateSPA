df0a1be88b52db99a0f8e20a9ffaf5214d4d89ca
milan.zivanov <milan.zivanov@gmail.com>
2017-11-04 20:31:18 +0100

    adding modal on click

--- index.js
+++ index.js
@@ -19,9 +19,35 @@ link.on("click", function(e) {
 });
 
 // arrow function in the action
-let clickBtn = document.getElementById("click");
-clickBtn.addEventListener("click", () => {
-    alert("Bu Bro!!!");
-    clickBtn.innerHTML = "CLICKED";
-});
+// let clickBtn = document.getElementById("modalBtn");
+// clickBtn.addEventListener("click", () => {
+//     alert("Bu Bro!!!");
+//     clickBtn.innerHTML = "CLICKED";
+// });
 
+// modal
+let modal = document.getElementById("simpleModal");
+let modalBtn = document.getElementById("modalBtn");
+let closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+// listen to open click
+modalBtn.addEventListener("click", openModal);
+// listen to close click
+closeBtn.addEventListener("click", closeModal);
+// listen to outside click
+window.addEventListener("click", clickOutside);
+
+// function to open modal
+function openModal() {
+    modal.style.display = "block";
+}
+// function to close modal
+function closeModal() {
+    modal.style.display = "none";
+}
+// // function to close modal when clicked outside
+function clickOutside(e) {
+    if(e.target === modal) {
+        modal.style.display = "none";
+    }
+}
